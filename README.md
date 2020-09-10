a657ebb00927364876828c6ba71bf0088235d45a
Ross Netterville <ross.netterville@gmail.com>
2020-09-10 12:17:22 +0200

    Started the Express api work

---
+++ README.md
@@ -0,0 +1,7 @@
+# Some notes on the approach taken when creating the application
+
+## Backend
+
+- Create an Express app and test that it's working
+- Set up the connection to the MongoDB database on Atlas
+- - create a config folder which has the connection URI and the actual db connection
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API running'));
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
