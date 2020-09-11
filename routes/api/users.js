46f8658f799366303c6128ee791653c1ad1526a3
Ross Netterville <ross.netterville@gmail.com>
2020-09-11 12:17:03 +0200

    added user creation in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
 router.post(
   '/',
+
+  //Some validation on the body content
   [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
@@ -15,12 +21,55 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ erros: errors.array() });
     }
-    res.send('User route');
+
+    //Pull the user details from the request body
+    const { name, email, password } = req.body;
+
+    try {
+      // Check to see if user already exists in the db
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get user's gravatar and set a default if it doesn't already exist
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      //create a new user (it's not saved yet)
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      //Save the user to the DB
+      await user.save();
+
+      //return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error: ' + err.message);
+    }
   }
 );
 
