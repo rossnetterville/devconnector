e0c6bd8606071bcd18e93770daddbbb719df68d6
Ross Netterville <ross.netterville@gmail.com>
2020-09-21 12:37:31 +0200

    Added Delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,4 +144,23 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile/
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // ToDo: remove users' posts
+
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove the user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
