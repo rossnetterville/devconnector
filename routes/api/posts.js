c36156f0f74f2e1dab04a193d5fcf7a07b9a5f3b
Ross Netterville <ross.netterville@gmail.com>
2020-10-19 17:13:56 +0200

    Created Add Post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,62 +1,40 @@
-// const express = require('express');
-// const router = express.Router();
-
-// // @route   GET api/posts
-// // @desc    Test route
-// // @access  Public
-// router.get('/', (req, res) => res.send('Posts route'));
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
+// @route   POST api/posts
+// @desc    Create a Post
+// @access  Private
 router.post(
   '/',
-
-  //Some validation on the body content
-  [
-    check('title', 'Name is required').not().isEmpty(),
-    check('body', 'Post Body is required').not().isEmpty(),
-  ],
-
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    //Pull the user details from the request body
-    const { title, body } = req.body;
-
     try {
-      // Check to see if user already exists in the db
-      let post = await Post.findOne({ title });
+      const user = await User.findById(req.user.id).select('-password');
 
-      if (post) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'Post already exists' }] });
-      }
-      //create a new user (it's not saved yet)
-      post = new Post({
-        title,
-        body,
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
       });
 
-      //Save the user to the DB
-      await post.save();
+      const post = await newPost.save();
 
-      res.send('Post Created');
+      res.json(post);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('server error: ' + err.message);
+      res.status(500).send('Server Error');
     }
   }
 );
