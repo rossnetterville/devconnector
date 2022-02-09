3d6ffbab9f648e7197b30eb14bc011dd3ce5f78b
Ross Netterville <ross_netterville@hotmail.com>
2022-02-09 11:33:06 +0200

    added createProfile action to the CreteProfile component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -20,3 +20,39 @@ export const getCurrentProfile = () => async dispatch => {
             payload: {msg: err.response.statusText, status: err.response.status}
         })
     }} 
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config)
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+        dispatch(setAlert(edit ? 'Profile Updated': 'profile created', 'success'))
+
+        // if it's a new profile and not an edit, redirect to the dashboard when complete
+        if(!edit){
+            history.push('/dashboard')
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors){
+            console.log(err.msg)
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}  
